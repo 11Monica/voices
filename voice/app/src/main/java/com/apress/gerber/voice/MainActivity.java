f480928b2e6497a3f592133c68946423e4942dec
11monica <1120541168@qq.com>
2017-08-17 21:50:49 +0000

    文件之前不全,提交全面的文件

--- voice/app/src/main/java/com/apress/gerber/voice/MainActivity.java
+++ voice/app/src/main/java/com/apress/gerber/voice/MainActivity.java
@@ -3,11 +3,15 @@ package com.apress.gerber.voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SpeechUtility.createUtility(this, SpeechConstant.APPID+"=5960ddce");
     }
 }
